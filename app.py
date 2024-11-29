dbb05ff7e0f60be19350fbc01cc9f4e198a582a6
Sayan Golder <sayangolder2004@gmail.com>
2024-11-29 15:24:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ app = Flask(__name__)
 
 account_sid = os.getenv('TWILIO_ACCOUNT_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-print(account_sid, auth_token)
 client = Client(account_sid, auth_token)
 
 UPLOAD_FOLDER = 'uploads' 
